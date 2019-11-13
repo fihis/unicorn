b02aa7711535c1c9a208ffac94316aa980b91d59
Fihis <isifuo@gmail.com>
2019-11-13 16:31:11 +0300

    feat: add task 6

--- 12-11/task6/script.js
+++ 12-11/task6/script.js
@@ -1,8 +1,17 @@
-window.onload = function() {
-    let cross = document.createElement('span');
-    cross.innerHTML = '[x]';
-    document.body.append(cross);
+window.onload = function () { 
     let messages = document.getElementsByClassName('message');
-    console.log(messages);
-
+    for (let i = 0; i < messages.length; i++){
+        let cross = document.createElement('span');
+        cross.innerHTML = '[x]';
+        cross.style.color = 'red';
+        cross.style.fontFamily = 'sans-serif';
+        cross.style.position = 'absolute';
+        cross.style.top = '5px';
+        cross.style.right = '5px';
+        cross.style.cursor = 'pointer';
+        cross.onclick = () => {
+            cross.parentElement.style.display = 'none'
+        }
+        messages[i].append(cross);
+    }
 }
