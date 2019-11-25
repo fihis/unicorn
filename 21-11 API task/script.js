acac5ff4df9d7bad32c5ece5125452fdf61aa92a
Fihis <isifuo@gmail.com>
2019-11-25 17:26:22 +0300

    feat: add search method

--- 21-11 API task/script.js
+++ 21-11 API task/script.js
@@ -3,9 +3,11 @@ window.onload = function () {
         constructor(url) {
             this.apiBaseUrl = url;
         }
+
         changeUrl(newUrl) {
             this.apiBaseUrl = newUrl;
         }
+
         async post() {
             this.method = 'POST';
             this.uri = '/json';
@@ -16,8 +18,8 @@ window.onload = function () {
             })
             this.result = await this.response.json();
             console.log(JSON.stringify(this.result));
-
         }
+
         async get() {
             this.method = 'GET';
             this.uri = '/json/1'
@@ -37,64 +39,33 @@ window.onload = function () {
             this.putInit = {
                 method: 'PUT',
                 headers: this.putHeaders,
-                
             }
-
             this.putRequest = new Request(this.apiBaseUrl + this.uri, this.putInit);
             this.response = await fetch(this.putRequest);
             this.result = await this.response.json();
             console.log(JSON.stringify(this.result));
         }
 
-        //not working
         async search() {
             this.uri = '/search';
             this.searchInit = {
-                method: 'GET',
-                query: 'q'
+                method: 'GET'
             }
             this.searchUrl = new URL(this.apiBaseUrl + this.uri);
-            // this.params = {
-            //     query: 'q'
-            // }
-            //this.searchUrl.searchParams.append('query', 'q');
-
-            this.searchRequest = new Request(this.apiBaseUrl + this.uri, this.searchInit);
+            this.searchUrl.searchParams.append('q','anyquery');
+            this.searchRequest = new Request(this.searchUrl, this.searchInit);
             this.response = await fetch(this.searchRequest);
-            this.result = await this.response.json();
-
-            console.log(JSON.stringify(this.result));
-
-
-            // this.response = await fetch(this.apiBaseUrl + this.uri, {
-            //     method: 'PUT',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     }
-            // });
-
+            this.result = await this.response.text();
+            console.log(this.result);
         }
     }
     const testAPI = new API('https://78jqd.mocklab.io');
-    //testAPI.post();
-    //testAPI.get();
-    //testAPI.put();
+    testAPI.post();
+    testAPI.get();
+    testAPI.put();
     testAPI.search();
 
 
 
 }
 
-
-// apiBaseUrl: http://78jqd.mocklab.io
-// endpoints:
-
-// method: PUT
-// uri: /json/2
-// description: Only match if Accept header is for application / json
-
-// method: GET
-// uri: /search
-// description: Only match if query parameter is present and in the correct format
-// queryParameterName: q
-// queryRestrictions: [A - Za - z0 - 9] *
