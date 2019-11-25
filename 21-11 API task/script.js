10eb8637fefb1d0224f61435fc7ad57f8dc3e63b
Fihis <isifuo@gmail.com>
2019-11-25 11:19:36 +0300

    feat: add put method

--- 21-11 API task/script.js
+++ 21-11 API task/script.js
@@ -2,11 +2,6 @@ window.onload = function () {
     class API {
         constructor(url) {
             this.apiBaseUrl = url;
-            this.postEndpoint = {
-                method: 'POST',
-                uri: '/json',
-                description: 'Match a JSON POST on the request body',
-            }
         }
         changeUrl(newUrl) {
             this.apiBaseUrl = newUrl;
@@ -14,7 +9,6 @@ window.onload = function () {
         async post() {
             this.method = 'POST';
             this.uri = '/json';
-            //this.description = 'Match a JSON POST on the request body';
             this.body = '{  "id": 12345,  "value": "abc-def-ghi" }';
             this.response = await fetch(this.apiBaseUrl + this.uri, {
                 method: this.method,
@@ -27,7 +21,6 @@ window.onload = function () {
         async get() {
             this.method = 'GET';
             this.uri = '/json/1'
-            //this.description = 'GET a JSON resource';
             this.response = await fetch(this.apiBaseUrl + this.uri, {
                 method: this.method,
                 description: this.description
@@ -36,54 +29,40 @@ window.onload = function () {
             console.log(JSON.stringify(this.result));
         }
 
-        //not working
         async put() {
+            //creating Init object to use with Fetch
             this.putHeaders = new Headers();
-            this.putHeaders['Content-Type'] = 'application/json';
+            this.putHeaders.append('Accept','application/json')
             this.uri = '/json/2';
-            console.log(this.putHeaders);
             this.putInit = {
                 method: 'PUT',
                 headers: this.putHeaders,
-                description: 'Only match if Accept header is for application / json',
+                
             }
+
             this.putRequest = new Request(this.apiBaseUrl + this.uri, this.putInit);
-            // this.method = 'PUT';
-            // this.description = 'Only match if Accept header is for application / json';
-            // this.header = 'application/json';
-
-
-            //this.response = await fetch(this.putRequest);
-
-
-            this.response = await fetch(this.apiBaseUrl + this.uri, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
+            this.response = await fetch(this.putRequest);
             this.result = await this.response.json();
             console.log(JSON.stringify(this.result));
-
         }
 
         //not working
         async search() {
             this.uri = '/search';
             this.searchInit = {
-                method: 'GET'
-                
-            }
-            this.searchUrl = new URL(this.apiBaseUrl + this.uri);
-            this.params = {
+                method: 'GET',
                 query: 'q'
             }
-            this.searchUrl.searchParams.append('query', 'q');
-            //Object.keys(this.params).forEach((key, value) => { this.searchUrl.searchParams.append(key, value) });
-            this.searchRequest = new Request(this.searchUrl, this.searchInit);
+            this.searchUrl = new URL(this.apiBaseUrl + this.uri);
+            // this.params = {
+            //     query: 'q'
+            // }
+            //this.searchUrl.searchParams.append('query', 'q');
+
+            this.searchRequest = new Request(this.apiBaseUrl + this.uri, this.searchInit);
             this.response = await fetch(this.searchRequest);
             this.result = await this.response.json();
+
             console.log(JSON.stringify(this.result));
 
 
